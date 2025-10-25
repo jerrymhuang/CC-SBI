aa86f4023ae4d9b9f6149bd86acd0b5f8b1f4d1e
jerrymhuang <aca.jerryh@gmail.com>
2025-10-25 13:32:11 -0400

    Add EVC Solver root

---
+++ src/solvers/evc_solver.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class EvcSolver:
+
+    def __init__(self):
+        super().__init__()
+
+
+    def solve_from_initial_guess(self):
+        # TODO: implement
+        raise NotImplementedError
